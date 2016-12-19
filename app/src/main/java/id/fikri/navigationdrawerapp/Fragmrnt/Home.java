0f74768604d445008a394d998cf09c24e060db0a
FikriFahruRoji <fikrifahruroji@live.com>
2016-12-19 11:39:27 +0700

    Second commit

---
+++ app/src/main/java/id/fikri/navigationdrawerapp/Fragmrnt/Home.java
@@ -0,0 +1,23 @@
+package id.fikri.navigationdrawerapp.Fragmrnt;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import id.fikri.navigationdrawerapp.R;
+
+/**
+ * Created by lowercase on 16/12/16.
+ */
+
+public class Home extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View viewRoot = inflater.inflate(R.layout.fragment_home, container, false);
+        return viewRoot;
+    }
+}
--- app/src/main/java/id/fikri/navigationdrawerapp/MainActivity.java
+++ app/src/main/java/id/fikri/navigationdrawerapp/MainActivity.java
@@ -1,9 +1,9 @@
 package id.fikri.navigationdrawerapp;
 
+import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,6 +12,12 @@ import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 
+import java.util.function.ToLongBiFunction;
+
+import id.fikri.navigationdrawerapp.Fragmrnt.Contact;
+import id.fikri.navigationdrawerapp.Fragmrnt.Home;
+import id.fikri.navigationdrawerapp.Fragmrnt.Profile;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
@@ -19,21 +25,24 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
+        Toolbar new_toolbar = (Toolbar) findViewById(R.id.toolbar);
+        new_toolbar.setTitle("Home");
+        setSupportActionBar(new_toolbar);
+
+        FragmentManager fr = getFragmentManager();
+        fr.beginTransaction().replace(R.id.content_fragment, new Home()).commit();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                //// TODO for fab action
             }
         });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, new_toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
@@ -54,15 +63,20 @@ public class MainActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
+        FragmentManager fr = getFragmentManager();
+        Toolbar new_toolbar = (Toolbar) findViewById(R.id.toolbar);
+
         int id = item.getItemId();
 
         if (id == R.id.nav_home) {
-            // Handle the camera action
+            fr.beginTransaction().replace(R.id.content_fragment, new Home()).commit();
+            new_toolbar.setTitle("Home");
         } else if (id == R.id.nav_profile) {
-
+            fr.beginTransaction().replace(R.id.content_fragment, new Profile()).commit();
+            new_toolbar.setTitle("Profile");
         } else if (id == R.id.nav_contact) {
-
+            fr.beginTransaction().replace(R.id.content_fragment, new Contact()).commit();
+            new_toolbar.setTitle("Contact");
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
